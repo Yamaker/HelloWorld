00275f8f06afdbeb16dcbb31736de16e79f5aea7
李诗雅 <shiya.li@ucarinc.com>
2020-03-10 16:39:40 +0800

    测试stash changes

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("f_20200310_test分支stash changes 测试");
+        System.out.println("f_20200310_test分支stash changes 测试2");
+
     }
 }
